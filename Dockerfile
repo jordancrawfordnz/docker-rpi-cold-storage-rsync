768335ec0ad9577518ca7cf9720e9b349eb8ec84
Jordan Crawford <jordan.crawford@me.com>
2016-03-07 10:04:50 +0100

    Add some more options for mounting

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ FROM resin/rpi-raspbian:jessie
 MAINTAINER Jordan Crawford <jordan.crawford@me.com>
 
 # Install rsync and the ability to use exfat drives.
-RUN apt-get update; apt-get install rsync exfat-fuse
+RUN apt-get update; apt-get install rsync exfat-fuse afpfs-ng
 
 # Setup the mount point of the backup device.
 RUN mkdir /backupdest
+RUN mkdir /backupsource
 
 # Copy the run script.
 ADD run.sh /root/run.sh
--- run.sh
+++ run.sh
@@ -1,10 +1,62 @@
 #!/bin/bash
 
-# Mount the ExFAT volume.
-mount /dev/sda1 /backupdest/
+function mountDrive {
+	local DRIVETYPE=$1;
+	local DRIVEOPTIONS=$2;
+	local MOUNTPT=$3;
+	echo "Mounting $DRIVETYPE. Options: $DRIVEOPTIONS. Mount at: $MOUNTPT.";
+
+	case $DRIVETYPE in
+		AFP)
+			mount_afp $DRIVEOPTIONS $MOUNTPT
+			;;
+		USB)
+			mount $DRIVEOPTIONS $MOUNTPT
+			;;
+		*)
+			echo "Unknown drive type!";
+			exit 1;
+	esac
+}
+
+# Check if environment variables are defined.
+if [ -z "${SOURCE_TYPE}" ]; then
+	echo "The required environment SOURCE_TYPE is not defined.";
+	exit 1;
+fi
+
+if [ -z "${SOURCE_OPTIONS}" ]; then
+        echo "The required environment SOURCE_OPTIONS is not defined.";
+        exit 1;
+fi
+
+if [ -z "${DEST_TYPE}" ]; then
+        echo "The required environment DEST_TYPE is not defined.";
+        exit 1;
+fi
+
+if [ -z "${DEST_OPTIONS}" ]; then
+        echo "The required environment DEST_OPTIONS is not defined.";
+        exit 1;
+fi
+
+if [ -z "${DEST_FOLDER}" ]; then
+        echo "The required environment DEST_FOLDER is not defined.";
+        exit 1;
+fi
+
+BACKUPSOURCE=/backupsource
+BACKUPDEST=/backupdest
+
+# Mount the source
+mountDrive $SOURCE_TYPE $SOURCE_OPTIONS $BACKUPSOURCE
+
+# Mount the destination
+mountDrive $DEST_TYPE $DEST_OPTIONS $BACKUPDEST
+
+BACKUPFULLSOURCE=$BACKUPSOURCE/
+BACKUPFULLDEST=$BACKUPDEST/$DEST_FOLDER/
+echo "Drives mounted. Starting rsync from $BACKUPFULLSOURCE to $BACKUPFULLDEST.";
 
 # Run a sync
-rsync --size-only -aPv --delete /backupsource/$FOLDER/ /backupdest/$FOLDER/
-
-# Unmount the ExFAT volume.
-umount /dev/sda1
+rsync --size-only -aPv --delete $BACKUPFULLSOURCE $BACKUPFULLDEST
